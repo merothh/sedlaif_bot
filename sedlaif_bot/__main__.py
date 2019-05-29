ed9bbb5d09e031900b9b80638b08b239305689a3
merothh <merothh@gmail.com>
2019-05-29 21:14:08 +0530

    sedlaif_bot: reply to /start in PM

--- sedlaif_bot/__main__.py
+++ sedlaif_bot/__main__.py
@@ -1,3 +1,18 @@
+from telegram.ext import Updater, CommandHandler
 from sedlaif_bot.config import Config
 
 TOKEN = Config.API_KEY
+
+updater = Updater(token=TOKEN)
+dispatcher = updater.dispatcher
+
+# callback function for start handler
+def start(bot, update):
+    if update.message.chat.type=="private":
+        bot.send_message(chat_id=update.message.chat_id, text="Hi there")
+
+start_handler = CommandHandler('start', start)
+
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
