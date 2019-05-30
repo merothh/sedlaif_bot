660e49dc035e690aa4b8a63c24490f225f27a7f4
merothh <merothh@gmail.com>
2019-05-30 18:05:09 +0530

    sedlaif_bot: implement space handler
    * reply to a message with sp/
    * for eg. "hi there" will be replied to as 'H  I     T  H  E  R  E'

--- sedlaif_bot/__main__.py
+++ sedlaif_bot/__main__.py
@@ -9,7 +9,6 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 
-
 # custom class to filter messages for echo handler
 class EchoFilter(BaseFilter):
     def filter(self, message):
@@ -20,6 +19,10 @@ class SedFilter(BaseFilter):
     def filter(self, message):
         split = message.text.split('/')
         return split[0] == 's' and len(split)>=3
+
+class SpaceFilter(BaseFilter):
+    def filter(self, message):
+        return message.text == 'sp/'
 
 # callback function for respective handlers
 def start_callback(bot, update):
@@ -61,6 +64,16 @@ def sed_callback(bot, update):
             reply=reply.replace(split[word],split[word+1],1)
         bot.send_message(chat_id=update.message.chat_id, text=reply, reply_to_message_id=replied_message_id)
 
+def space_callback(bot, update):
+    replied_message = is_reply(update)[0]
+    replied_message_id = is_reply(update)[1]
+    bot.delete_message(chat_id=update.message.chat_id, message_id=update.message.message_id)
+
+    reply=''
+    for char in replied_message:
+        reply+=char.upper()+"  "
+    bot.send_message(chat_id=update.message.chat_id, text=reply, reply_to_message_id=replied_message_id)
+
 # checks if message is a reply to another message
 def is_reply(update):
     if(update.message.reply_to_message is not None):
@@ -72,13 +85,16 @@ def is_reply(update):
 
 echo_filter = EchoFilter()
 sed_filter = SedFilter()
+space_filter = SpaceFilter()
 
 start_handler = CommandHandler('start', start_callback)
 echo_handler = MessageHandler(echo_filter, echo_callback)
 sed_handler = MessageHandler(sed_filter, sed_callback)
+space_handler = MessageHandler(space_filter, space_callback)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(echo_handler)
 dispatcher.add_handler(sed_handler)
+dispatcher.add_handler(space_handler)
 
 updater.start_polling()
